9003e1ef0f321812ad04487906a761febf522900
som-syom <dhyeon@student.42seoul.kr>
2022-02-15 03:12:10 +0900

    feat: useMedia hooks 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,11 @@ import Main from 'components/Main';
 import About from 'components/About';
 import DownloadButton from 'components/common/DownloadButton';
 import LiveVideo from 'components/LiveVideo';
+import useMedia from 'utils/hooks/useMedia';
 
 const Home: NextPage = () => {
+  const { isTablet, isPc } = useMedia();
+
   return (
     <div>
       <Navigation />
