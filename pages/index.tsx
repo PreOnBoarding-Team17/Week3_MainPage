e503d82416ec8ea541009784229f567fb4db7a0e
som-syom <dhyeon@student.42seoul.kr>
2022-02-15 00:41:50 +0900

    feat #5: DownloadBtn 구현

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,16 @@
 import type { NextPage } from "next";
 import Navigation from "components/common/Navigation";
-import Main from 'components/Main'
+import Main from "components/Main";
+import DownloadButton from "components/common/DownloadButton";
 
 const Home: NextPage = () => {
   return (
     <div>
       <Navigation />
+      <DownloadButton />
       <Main />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
