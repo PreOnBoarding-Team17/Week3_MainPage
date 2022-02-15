252b4999ab1045121d4149eeda8771217ffdd784
[2기-2반] 황상섭 <sshwang12345@gmail.com>
2022-02-15 19:32:23 +0900

    refactor: About 컴포넌트 리팩토링

--- components/About/index.tsx
+++ components/About/index.tsx
@@ -1,5 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
+import { AboutText } from 'utils/constants';
+
+interface AboutProps {
+  isPc: boolean;
+}
+
+const About = ({ isPc }: AboutProps) => {
+  return (
+    <AboutContainer>
+      <Image src="/assets/second-logo.gif" alt="logo" />
+      <AboutTitle>땅콩스쿨이란?</AboutTitle>
+      <AboutContent>{AboutText(isPc)}</AboutContent>
+    </AboutContainer>
+  );
+};
 
 const AboutContainer = styled.div`
   box-sizing: border-box;
@@ -7,10 +22,8 @@ const AboutContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-
   padding: 90px 20px 100px 20px;
   color: #fff;
-
   @media ${({ theme }) => theme.device.tablet} {
     padding: 130px 30px 150px 30px;
   }
@@ -19,7 +32,6 @@ const AboutContainer = styled.div`
 const Image = styled.img`
   width: 67px;
   height: 50px;
-
   @media ${({ theme }) => theme.device.tablet} {
     width: 134px;
     height: 100px;
@@ -29,7 +41,6 @@ const Image = styled.img`
 const AboutTitle = styled.div`
   font-size: 30px;
   margin-bottom: 45px;
-
   @media ${({ theme }) => theme.device.tablet} {
     font-size: 50px;
     margin-bottom: 70px;
@@ -41,33 +52,10 @@ const AboutContent = styled.div`
   text-align: center;
   font-size: 16px;
   line-height: 1.75;
-
   @media ${({ theme }) => theme.device.tablet} {
     font-size: 24px;
     line-height: 1.67;
   }
 `;
-const ABOUTCONTENTTEXT = [
-  '실시간(LIVE)으로, 아이들이 좋아하는, 캐릭터 선생님을 통해,\n 친구들과 함께 창의독서, 퀴즈 등을 재미있게 즐길 수 있는 온라인 서비스입니다.',
-  '실시간(LIVE)으로, 아이들이 좋아하는\n 캐릭터 선생님을 통해,\n 친구들과 함께 창의독서, 퀴즈 등을\n 재미있게 즐길 수 있 는 온라인 서비스입니다.',
-];
-
-interface AboutProps {
-  isPc: boolean;
-}
-
-const About: React.FC<AboutProps> = ({ isPc }) => {
-  return (
-    <AboutContainer>
-      <Image src="/assets/second-logo.gif" alt="logo" />
-
-      <AboutTitle>땅콩스쿨이란?</AboutTitle>
-
-      <AboutContent>
-        {isPc ? ABOUTCONTENTTEXT[0] : ABOUTCONTENTTEXT[1]}
-      </AboutContent>
-    </AboutContainer>
-  );
-};
 
 export default About;
--- utils/constants/index.ts
+++ utils/constants/index.ts
@@ -89,6 +89,14 @@ const LiveVideoText = (isPc: boolean) => {
   }
 };
 
+const AboutText = (isPc: boolean) => {
+  if (isPc) {
+    return `실시간(LIVE)으로, 아이들이 좋아하는, 캐릭터 선생님을 통해,\n 친구들과 함께 창의독서, 퀴즈 등을 재미있게 즐길 수 있는 온라인 서비스입니다.`;
+  } else {
+    return `실시간(LIVE)으로, 아이들이 좋아하는\n 캐릭터 선생님을 통해,\n 친구들과 함께 창의독서, 퀴즈 등을\n 재미있게 즐길 수 있는 온라인 서비스입니다.`;
+  }
+};
+
 export {
   ReadingBooksTitle,
   ReadingBooksText,
@@ -101,4 +109,5 @@ export {
   CardThirdText,
   LiveVideoTitle,
   LiveVideoText,
+  AboutText,
 };
